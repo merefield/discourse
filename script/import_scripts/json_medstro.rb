8974610e165da32360b1c24f752e7d7b7d2e1e81
merefield <merefield@github.com>
2020-10-15 18:07:06 +0000

    added categories with hierarchy

--- script/import_scripts/json_medstro.rb
+++ script/import_scripts/json_medstro.rb
@@ -82,16 +82,16 @@ class ImportScripts::JsonGeneric < ImportScripts::Base
       end
     end
 
+    top_level.each do |top_level|
+      subset.push(top_level)
+    end
+
     master.each do |master_record|
       top_level.each do |top_level|
         if master_record['parent_group_id'] == top_level['id'] #|| master_record['id'] == 203
           subset.push(master_record)
         end
       end
-    end
-
-    top_level.each do |top_level|
-      subset.push(top_level)
     end
 
     puts subset
@@ -221,130 +221,56 @@ class ImportScripts::JsonGeneric < ImportScripts::Base
   def import_categories
     puts "", "Importing Categories"
 
-    top_level_categories = []
-    mid_level_categories = []
-    child_categories = []
+    categories = []
+    leaf_categories = []
     eligible = []
 
-    # Group.all.each do |group|
-    #   if group.automatic == false
-    #     eligible.push(group.custom_fields["import_id"])
-    #   end
-    # end
+    puts "", "Importing Categories"
 
-    puts "", "Importing Top Level Categories"
-
-    @imported_group_json.each do |top_level_category|
-      if top_level_category['parent_group_id'] == 203
-        top_level_categories.push(top_level_category)
-        eligible.push(top_level_category['id'])
-      end
+    @imported_group_json.each do |category|
+      categories.push(category)
+      eligible.push(category['id'])
     end
 
-    top_level_categories.uniq!
-
-    puts top_level_categories[0]
-
-    puts "", "Found #{top_level_categories.count} Top Level Categories"
     puts "", "Found #{eligible.count} Eligible Parent Categories so far"
-
-    Category.transaction do
-      create_categories(top_level_categories) do |g|
-        {
-          id: g['id'],
-          name: g['name'],
-          description: g['description'],
-          created_at: Time.now,
-          updated_at: Time.now,
-          post_create_action: proc do |newcategory|
-            newcategory.custom_fields["import_group_id"] = g['id'].to_s
-            newcategory.save!
-          end
-        }
-      end
-    end
-
-    @imported_group_json.each do |mid_level_category|
-      if mid_level_category['parent_group_id'] != 203
-        mid_level_categories.push(mid_level_category)
-        eligible.push(mid_level_category['id'])
-      end
-    end
-
-    mid_level_categories.uniq!
-
-    puts mid_level_categories[0]
-
-    puts "", "Found #{mid_level_categories.count} Mid Level Categories"
-    puts "", "Found #{eligible.count} Eligible Parent Categories so far"
-
-
-    Category.transaction do
-      create_categories(mid_level_categories) do |g|
-        {
-          id: g['id'],
-          name: g['name'],
-          description: g['description'],
-          created_at: Time.now,
-          updated_at: Time.now,
-          post_create_action: proc do |newcategory|
-            newcategory.custom_fields["import_group_id"] = g['id'].to_s
-            newcategory.custom_fields["import_parent_group_id"] = g['parent_group_id'].to_s
-            newcategory.save!
-          end
-        }
-      end
-    end
-
-    puts "", "Importing Child Categories"
 
     @imported_discussion_json.each do |child_category|
       if eligible.include?(child_category['group_id'].to_s)
-        # Group.all.each do |g|
-        #   if g.custom_fields["import_id"] == category['parent_group_id'].to_s
-            #byebug
-            #category['import_parent_group_id'] = g.custom_fields["import_parent_group_id"]
-        child_categories.push(child_category)
-            # break
-          # end
-        # end
-      end
-    end
-    
-    child_categories.uniq!
-
-    puts child_categories[0]
-
-    puts "", "Found #{child_categories.count} Child Level Categories"
-
-    Category.transaction do
-      create_categories(child_categories) do |c|
-        {
-          id: c['id'],
-          name: c['title'],
-          description: c['description'],
-          created_at: Time.now,
-          updated_at: Time.now,
-          post_create_action: proc do |newcategory|
-            newcategory.custom_fields["import_parent_group_id"] = c['import_parent_group_id'].to_s
-            newcategory.custom_fields["import_group_id"] = c['group_id'].to_s
-            newcategory.save!
-          end
-        }
+        child = Hash.new
+        child['id'] = child_category['id']
+        child['parent_group_id'] = child_category['group_id']
+        child['name'] =  child_category['title']
+        child['description'] = child_category['description']
+        categories.push(child)
+        leaf_categories.push(child)
       end
     end
 
-    Category.transaction do
-      Category.all.each do |category|
-        if category.custom_fields["import_parent_group_id"]
-          Category.all.each do |potential_parent_category|
-            if potential_parent_category.custom_fields["import_group_id"] == category.custom_fields["import_parent_group_id"]
-              byebug
-              category.parent_category_id = potential_parent_category.id
-            end
-          end
+    puts "", "Found #{leaf_categories.count} Leaf Categories"
+
+    categories.uniq!
+
+    puts categories
+
+    puts "", "Found #{categories.count} Categories"
+
+    create_categories(categories) do |gd|
+      h = {
+        id: gd['id'],
+        name: gd['name'],
+        description: gd['description'],
+        created_at: Time.now,
+        updated_at: Time.now,
+        post_create_action: proc do |newcategory|
+          newcategory.custom_fields["import_group_id"] = gd['id'].to_s
+          newcategory.custom_fields["import_parent_group_id"] = gd['parent_group_id'].to_s
+          newcategory.save!
         end
+      }
+      if gd['parent_group_id'].to_i > 0 && gd['parent_group_id'].to_i != 203
+        h[:parent_category_id] = category_id_from_imported_category_id(gd['parent_group_id'])
       end
+      h
     end
   end
 
